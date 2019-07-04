d487e3fb1b4e8c05aa31bbbb5b2c1eebc0f50d36
ksm240 <kankichi69@gmail.com>
2019-07-04 14:53:54 +0900

    Learn Class

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Human extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { name : "ksm240" };
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render () {
+    return (
+      <h2 onClick={this.onButtonClick}>
+        {this.state.name} {this.props.age}
+      </h2>  
+    );
+  }
+
+  onButtonClick = () => {
+    this.setState({ name: this.state.name + "san"});
+  }
+}
+
+
+render(<Human age="30"/>, document.getElementById('root'));
